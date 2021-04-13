e69d9b77bee586a7b8e564f24053fddc600c4377
fanxiaogang <fanxiaogang@jd.com>
2021-04-13 11:28:22 +0800

    引入单测代码api

---
+++ unit-test-api/src/main/java/unit/test/api/Test3.java
@@ -0,0 +1,76 @@
+package unit.test.api;
+
+import java.util.Date;
+
+/**
+ * @author xiaogangfan
+ * @date 2019/12/31
+ */
+public class Test3 {
+
+    public Test3() {
+
+    }
+
+    private String aa;
+    private Integer bb;
+    private Date cc;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    public Integer getBb() {
+        return bb;
+    }
+
+    public void setBb(Integer bb) {
+        this.bb = bb;
+    }
+
+    public Date getCc() {
+        return cc;
+    }
+
+    public void setCc(Date cc) {
+        this.cc = cc;
+    }
+
+    public static class Test2 {
+        public Test2() {
+
+        }
+
+        private String aa;
+        private Integer bb;
+        private Date cc;
+
+        public String getAa() {
+            return aa;
+        }
+
+        public void setAa(String aa) {
+            this.aa = aa;
+        }
+
+        public Integer getBb() {
+            return bb;
+        }
+
+        public void setBb(Integer bb) {
+            this.bb = bb;
+        }
+
+        public Date getCc() {
+            return cc;
+        }
+
+        public void setCc(Date cc) {
+            this.cc = cc;
+        }
+    }
+}
