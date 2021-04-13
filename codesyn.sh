399a87abe47e7d2c5971d5ec4f2e41863c51008a
fanxiaogang <fanxiaogang@jd.com>
2021-04-13 11:10:30 +0800

    调整代码的骨架

--- codesyn.sh
+++
@@ -1,5 +0,0 @@
-rm -rf *
-cp -rf ~/code_git/private/unit-test/* .
-git pull --rebase
-git add .
-./git.sh 'code syn'
--- idea-plugin/generatePlugin.sh
+++ idea-plugin/generatePlugin.sh
@@ -1,4 +1,5 @@
 echo "请确保已经安装了Gradle"
 gradle assemble
+rm -rf unit.test-*
 cp build/distributions/* .
 echo "插件生成成功，在项目根目录下"
